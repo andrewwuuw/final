2c3a94b61113b98f7602c25b571651ec1385ef16
chiaqingwu <secret8419@gmail.com>
2018-07-04 14:04:20 +0800

    修改忘記密碼功能，已完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,21 +29,12 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'http://pure-depths-74060.herokuapp.com' }
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
-  config.action_mailer.smtp_settings = {
-　　:address => "smtp.gmail.com",
-  　:port => "587",
-    :domain => "yourdomain.com",
-    :authentication => "plain",
-    :user_name => "yourgmail@gmail.com",
-    :password => "yourpassword",
-    :enable_starttls_auto => true
-  }
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'final <10446012@ntub.edu.tw>'
+  config.mailer_sender = 'gowhere.ntub@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
